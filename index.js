2083342f8a986afd601dfdaaa06562437f1df3ae
Saurabh Mishra <hey.saurabh.m@gmail.com>
2022-08-22 14:39:58 +0530

    Version 2
    Added functionalities - 
    1. You can enter player names. (Only the first time when you're starting)
    2. Added a button to play a new game, instead of refreshing the whole page.

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
-var random1 = 1 + Math.floor(Math.random()*6);
-var random2 = 1 + Math.floor(Math.random()*6);
+var player1 = prompt("Enter the name of first player.");
+var player2 = prompt("Enter the name of second player.");
 
-console.log(random1+" "+random2);
-var ans = "";
+function newGame(){
+  var random1 = 1 + Math.floor(Math.random()*6);
+  var random2 = 1 + Math.floor(Math.random()*6);
 
-if(random1 == random2){
-  ans = "Draw";
-}
-else if(random1 > random2){
-  ans = "🚩Player 1 Wins!";
-}
-else{
-  ans = "Player 2 Wins!🚩";
+  console.log(random1+" "+random2);
+  var ans = "";
+
+  if(random1 == random2){
+    ans = "Draw";
+  }
+  else if(random1 > random2){
+    ans = `🚩${player1} Wins!`;
+  }
+  else{
+    ans =  `${player2} Wins! 🚩`;
+  }
+
+  document.querySelector("h2").innerHTML = ans;
+  document.querySelector(".img1").setAttribute("src", `images/dice${random1}.png`);
+  document.querySelector(".img2").setAttribute("src", `images/dice${random2}.png`);
 }
 
-document.querySelector("h1").innerHTML = ans;
-document.querySelector(".img1").setAttribute("src", `images/dice${random1}.png`);
-document.querySelector(".img2").setAttribute("src", `images/dice${random2}.png`);
+newGame();
+
+document.querySelector(".btn").addEventListener('click', newGame );
